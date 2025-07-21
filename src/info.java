120fc5ab29faa578d522f588b8a44866fdac7fc8
đungayne <hihia174@gmail.com>
2025-07-21 16:42:53 +0700

    update

--- src/info.java
+++ src/info.java
@@ -1,6 +1,6 @@
 public class info {
     public static void main(String[] args) {
-        System.out.println("dungnehihi");
+        System.out.println("dungngit hi");
         hihih a = new hihih();
     }
 }
