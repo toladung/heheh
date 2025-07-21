539c6eae7f39f0c4a022cd9c0189287f6ec52d97
toladung <161729512+toladung@users.noreply.github.com>
2025-07-21 17:19:48 +0700

    Update info.java

--- src/info.java
+++ src/info.java
@@ -1,6 +1,6 @@
 public class info {
     public static void main(String[] args) {
-        System.out.println("dungnehehegitikehihi");
+        System.out.println("dungikehihi");
         hihih a = new hihih();
     }
 }
