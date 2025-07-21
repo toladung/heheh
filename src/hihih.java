47ceaf3ebbfc289f41d04a861212a5adbac38d02
đungayne <hihia174@gmail.com>
2025-07-21 15:46:00 +0700

    heeh

---
+++ src/hihih.java
@@ -0,0 +1,20 @@
+public class hihih {
+    String name ;
+    int tuoi;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTuoi() {
+        return tuoi;
+    }
+
+    public void setTuoi(int tuoi) {
+        this.tuoi = tuoi;
+    }
+}
--- src/info.java
+++ src/info.java
@@ -1,5 +1,7 @@
 public class info {
     public static void main(String[] args) {
         System.out.println("dungnehihi");
+        hihih a = new hihih();
+
     }
 }
